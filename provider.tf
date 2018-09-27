aaa35aebc2a1ed63e2efcdf07b27abc4b5eb0325
stevestrutt <steve_strutt@uk.ibm.com>
2018-09-27 10:17:20 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,8 @@
-variable "iaas_username" {
-  description = "Enter your IBM IaaS Infrastructure full username, you can get this using: https://control.bluemix.net/account/user/profile"
-}
+# variables supplied from terraform.tfvars
 
-variable "ibmcloud_iaas_api_key" {
-  description = "Enter your IBM IaaS Infrastructure API key, you can get this using: https://control.bluemix.net/account/user/profile"
-}
-
-variable "ibmcloud_api_key" {
-  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys"
-}
+variable "iaas_username" {}
+variable "ibmcloud_iaas_api_key" {}
+variable "ibmcloud_api_key" {}
 
 provider "ibm" {
   softlayer_username = "${var.iaas_username}"
