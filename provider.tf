efa26b13a5189ab357688fdfe988f45a413f78c6
stevestrutt <steve_strutt@uk.ibm.com>
2018-09-27 10:13:00 +0100

    Update to IBM Cloud

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
-variable "softlayer_username" {
-  description = "Enter your IBM Infrastructure (SoftLayer) full username, you can get this using: https://control.bluemix.net/account/user/profile"
+variable "iaas_username" {
+  description = "Enter your IBM IaaS Infrastructure full username, you can get this using: https://control.bluemix.net/account/user/profile"
 }
 
-variable "softlayer_api_key" {
-  description = "Enter your IBM Infrastructure (SoftLayer) API key, you can get this using: https://control.bluemix.net/account/user/profile"
+variable "ibmcloud_iaas_api_key" {
+  description = "Enter your IBM IaaS Infrastructure API key, you can get this using: https://control.bluemix.net/account/user/profile"
 }
 
 variable "ibmcloud_api_key" {
@@ -11,7 +11,7 @@ variable "ibmcloud_api_key" {
 }
 
 provider "ibm" {
-  softlayer_username = "${var.softlayer_username}"
-  softlayer_api_key  = "${var.softlayer_api_key}"
+  softlayer_username = "${var.iaas_username}"
+  softlayer_api_key  = "${var.ibmcloud_iaas_api_key}"
   bluemix_api_key    = "${var.ibmcloud_api_key}"
 }
